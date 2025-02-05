44bf25f096d7cf551ed178c3f5dd3ff8f6cf43f0
Ray <startup@9043672@gmail.com>
2025-02-05 19:56:35 +0800

    chore:檔案引入tailwind/firebase

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'flowbite';
+import '@/assets/index.css'
 
 const app = createApp(App)
 
